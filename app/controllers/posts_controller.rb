792dd819faea58a8b2ec465b406d24f5fa19f263
Scott Southard <scottmsouthard@gmail.com>
2016-07-25 16:07:01 -0500

    Add secondary calendar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.reverse
-    @upcomingposts = Post.where(date > Date.now)
+    @upcomingposts = Post.where("date > ?", Date.today)
     @date = params[:month] ? Date.parse(params[:month].gsub('-', '/')) : Date.today
   end
 
