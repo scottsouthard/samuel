a9f8eadb4f4505cdb266c7c565ff5eeb40bca0a3
scottsouthard <scottmsouthard@gmail.com>
2016-08-15 19:53:39 -0500

    Tweak environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('samuelgallery.herokuapp.com'),
-      access_key_id: ENV.fetch('AKIAIQNOHPDEVXKRINBA'),
-      secret_access_key: ENV.fetch('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'),
-      s3_region: ENV.fetch('us_standard'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
   if ENV["RAILS_LOG_TO_STDOUT"].present?
